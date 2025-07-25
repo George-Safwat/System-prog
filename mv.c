70a5d3bb89eb2ebeaed5450d226207ede7bd7b6d
George Safwat <georgesafwat779@gmail.com>
2025-07-25 12:59:41 +0300

    mv.c

---
+++ mv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int mv_main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: mv <source> <destination>\n");
+        return 1;
+    }
+
+    if (rename(argv[1], argv[2]) != 0) {
+        perror("mv");
+        return 1;
+    }
+
+    return 0;
+}
